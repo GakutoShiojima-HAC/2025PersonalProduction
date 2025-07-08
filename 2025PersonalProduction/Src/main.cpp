a9748591721e5fec68a1c0344509cda6bf55da7b
Gakuto <G023G1022@g.neec.ac.jp>
2025-07-08 22:31:13 +0900

    コメントの修正

--- 2025PersonalProduction/Src/main.cpp
+++ 2025PersonalProduction/Src/main.cpp
@@ -1,9 +1,9 @@
 // -----------------------------------------------------------------------------------------
-//  File        : main.cpp
-//  Author      : Shiojima Gakuto
-//  Created     : 2025/07/08
-//  Updated     : 2025/07/08
-//  Description : アプリケーション動作用クラス
+//  File          : main.cpp
+//  Author        : Shiojima Gakuto
+//  Created       : 2025/07/08
+//  Updated       : 2025/07/08
+//  Description   : アプリケーションとして動作させるための機能を持つクラス
 //
 //  注意：本ソースコードの無断転載・コードのコピー・貼り付けによる流用・再配布を禁止します。
 // -----------------------------------------------------------------------------------------
@@ -14,36 +14,21 @@
 
 class MyGame : public gslib::Game {
 public:
-    /// <summary>
-    /// アプリケーションコンストラクタ
-    /// </summary>
     MyGame() : gslib::Game{ 640, 480, false, 60.0f } { }
 
 private:
-    /// <summary>
-    /// アプリケーションの開始
-    /// </summary>
     void start() override {
 
     }
 
-    /// <summary>
-    /// アプリケーションの更新
-    /// </summary>
     void update(float delta_time) override {
     
     }
 
-    /// <summary>
-    /// アプリケーションの描画
-    /// </summary>
     void draw() override {
     
     }
 
-    /// <summary>
-    /// アプリケーションの終了
-    /// </summary>
     void end() override {
         // カーソルの移動制限を解除
         ClipCursor(NULL);
@@ -52,8 +37,9 @@ private:
     }
 
     /// <summary>
-    /// アプリケーションの強制終了条件
+    /// アプリケーションの実行が継続するかどうか
     /// </summary>
+    /// <returns>実行を継続するなら真</returns>
     bool is_running() override {
         // TODO 終了条件
         
