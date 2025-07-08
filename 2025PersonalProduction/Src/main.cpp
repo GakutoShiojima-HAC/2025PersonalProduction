a29947d5ab9146abd2df0417a2219f0e95d31187
Gakuto <G023G1022@g.neec.ac.jp>
2025-07-09 00:25:41 +0900

    シーンマネージャーをアプリケーションに組み込み

--- 2025PersonalProduction/Src/main.cpp
+++ 2025PersonalProduction/Src/main.cpp
@@ -2,7 +2,7 @@
 //  File          : main.cpp
 //  Author        : Shiojima Gakuto
 //  Created       : 2025/07/08
-//  Updated       : 2025/07/08
+//  Updated       : 2025/07/09
 //  Description   : アプリケーションとして動作させるための機能を持つクラス
 //
 //  注意：本ソースコードの無断転載・コードのコピー・貼り付けによる流用・再配布を禁止します。
@@ -11,25 +11,37 @@
 #include <GSgame.h>
 #include <GSeffect.h>
 #include <GSmovie.h>
+#include "Engine/Core/Scene/SceneManager.h"
 
 class MyGame : public gslib::Game {
 public:
-    MyGame() : gslib::Game{ 640, 480, false, 60.0f } { }
+    /// <summary>
+    /// アプリケーションコンストラクタ
+    /// </summary>
+    /// <param name="width_px">= ウィンドウの初期幅(ピクセル)</param>
+    /// <param name="height_px">= ウィンドウの初期高さ(ピクセル)</param>
+    /// <param name="full_screen">= フルスクリーンにするなら真</param>
+    /// <param name="refresh_rate">= アプリケーションのリフレッシュレート</param>
+    MyGame(int width_px, int height_px, bool full_screen, float refresh_rate) : gslib::Game{ width_px, height_px, full_screen, refresh_rate } {
+        scene_manager_.init();
+    }
 
 private:
     void start() override {
-
+        
     }
 
     void update(float delta_time) override {
-    
+        scene_manager_.update(delta_time);
     }
 
     void draw() override {
-    
+        scene_manager_.draw();
     }
 
     void end() override {
+        scene_manager_.clear();
+
         // カーソルの移動制限を解除
         ClipCursor(NULL);
         // カーソルを表示
@@ -41,12 +53,25 @@ private:
     /// </summary>
     /// <returns>実行を継続するなら真</returns>
     bool is_running() override {
-        // TODO 終了条件
+        // TODO 強制終了条件
         
+        // シーンからの終了リクエスト
+        if (scene_manager_.is_application_end()) return false;
+
         return true;
     }
+
+private:
+    // シーンマネージャー
+    SceneManager& scene_manager_ = SceneManager::get_instance();
+
 };
 
 int main() {
-    return MyGame().run();
+#ifdef _DEBUG
+    return MyGame(1920, 1080, false, 60.0f).run();
+#else
+    return MyGame(1920, 1080, true, 60.0f).run();
+#endif
+
 }
