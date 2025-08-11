7ccb589575e7cee6b9609fe724f2b636c1d6e797
Gakuto <G023G1022@g.neec.ac.jp>
2025-08-12 04:55:33 +0900

    攻撃判定のオブジェクトプールを追加

---
+++ 2025PersonalProduction/Src/Engine/Core/Collision/AttackColliderPool.cpp
@@ -0,0 +1,39 @@
+#include "Engine/Core/Collision/AttackColliderPool.h"
+#include "Engine/Core/Collision/AttackCollider.h"
+#include "Engine/Core/World/World.h"
+
+AttackColliderPool::AttackColliderPool(World* world) {
+	world_ = world;
+}
+
+AttackColliderPool::~AttackColliderPool() {
+	clear();
+}
+
+void AttackColliderPool::generate(float radius, const GSvector3& center, Actor* owner, int damage, float lifespan, float delay) {
+	AttackCollider* collider = colliders_.get();
+	collider->reactivate(radius, center, owner, damage, lifespan, delay, true);
+	active_colliders_.push_back(collider);
+	world_->add_actor(collider);
+}
+
+void AttackColliderPool::remove() {
+	for (auto i = active_colliders_.begin(); i != active_colliders_.end();) {
+		if ((*i)->is_dead()) {
+			// マネージャーからのメモリ解放を無効化しているならプール管理下に戻す
+			if (!(*i)->is_clear()) colliders_.release(*i);
+			i = active_colliders_.erase(i);
+		}
+		else {
+			++i;
+		}
+	}
+}
+
+void AttackColliderPool::clear() {
+	for (auto& collider : active_colliders_) {
+		// マネージャーからのメモリ解放を無効化しているならプール管理下に戻す
+		if (!collider->is_clear()) colliders_.release(collider);
+	}
+	active_colliders_.clear();
+}
---
+++ 2025PersonalProduction/Src/Engine/Core/Collision/AttackColliderPool.h
@@ -0,0 +1,61 @@
+// -----------------------------------------------------------------------------------------
+//  File          : AttackColliderPool.h
+//  Author        : Shiojima Gakuto
+//  Created       : 2025/08/12
+//  Updated       : 2025/08/12
+//  Description   : 攻撃判定のプール
+//					メモリ解放はプール内で行っているため不要
+//
+//  注意：本ソースコードの無断転載・コードのコピー・貼り付けによる流用・再配布を禁止します。
+// -----------------------------------------------------------------------------------------
+
+#ifndef ATTACK_COLLIDER_POOL_H_
+#define ATTACK_COLLIDER_POOL_H_
+
+#include "Engine/Utils/ObjectPool.h"
+#include <list>
+
+using namespace std;
+
+class World;
+class AttackCollider;
+
+class AttackColliderPool {
+public:
+	AttackColliderPool(World* world);
+
+	~AttackColliderPool();
+
+public:
+	void remove();
+
+	void clear();
+
+public:
+	/// <summary>
+	/// 攻撃判定を生成
+	/// </summary>
+	/// <param name="radius">= 判定の半径</param>
+	/// <param name="center">= 生成判定球の中心</param>
+	/// <param name="owner">= 生成主</param>
+	/// <param name="damage">= ダメージ値</param>
+	/// <param name="lifespan">= 判定の寿命</param>
+	/// <param name="delay">= 判定が有効になるまでの遅延</param>
+	void generate(float radius, const GSvector3& center, Actor* owner, int damage, float lifespan = 1.0f, float delay = 0.0f);
+
+private:
+	World* world_{ nullptr };
+
+	// コライダープール
+	ObjectPool<AttackCollider> colliders_;
+	// アクティブなコライダーリスト
+	list<AttackCollider*> active_colliders_;
+
+public:
+	// コピー禁止
+	AttackColliderPool(const AttackColliderPool& other) = delete;
+	AttackColliderPool& operator = (const AttackColliderPool& other) = delete;
+
+};
+
+#endif
