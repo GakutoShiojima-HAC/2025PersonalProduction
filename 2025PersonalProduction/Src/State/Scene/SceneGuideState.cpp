d6c7a2fd36ec8389a25bac8b8ee6414f436e5e8d
Gakuto <G023G1022@g.neec.ac.jp>
2025-10-22 02:26:36 +0900

    操作説明画面に戻るボタンを追加

--- 2025PersonalProduction/Src/State/Scene/SceneGuideState.cpp
+++ 2025PersonalProduction/Src/State/Scene/SceneGuideState.cpp
@@ -4,32 +4,40 @@
 #include "Engine/Graphics/Canvas/Canvas.h"
 #include "Assets.h"
 #include "Engine/Core/Input/Input.h"
+#include "GUI/Button/TextFunctionButton.h"
 
-// „Éë„ÉÉ„Éâ„Ç¢„Ç§„Ç≥„É≥„ÅÆÊã°Á∏Æ„Çπ„Éî„Éº„Éâ
+// ÉpÉbÉhÉAÉCÉRÉìÇÃägèkÉXÉsÅ[Éh
 static const float GAMEPAD_SCALE_SPEED{ 0.05f };
-// „Éë„ÉÉ„Éâ„Ç¢„Ç§„Ç≥„É≥„ÅÆÊúÄÂ§ßÊã°ÂºµÈáè(„ÇÇ„Å®„Å´Âä†ÁÆó)
+// ÉpÉbÉhÉAÉCÉRÉìÇÃç≈ëÂägí£ó (Ç‡Ç∆Ç…â¡éZ)
 static const float GAMEPAD_MAX_SCALE{ 0.1f };
 
 SceneGuideState::SceneGuideState(StandardScene& owner, SceneStateType return_scene) :
     owner_{ owner },
     return_scene_{ return_scene } {
-
+    // É{É^ÉìÇÃí«â¡
+    {
+        TextFunctionButton* button = new TextFunctionButton{ "ñﬂÇÈ", GSvector2{ 1588.0f, 908.0f }, 60, Anchor::TopLeft, Anchor::Center };
+        button->on_input([=] { owner_.change_state((GSuint)return_scene_); });
+        button_.add(button);
+    }
 }
 
 void SceneGuideState::enter() {
     scale_gamepad_ = 0.0f;
+    button_.start();
 }
 
 void SceneGuideState::update(float delta_time) {
     Input& input = Input::get_instance();
 
-    // Âº∑Âà∂ÁµÇ‰∫Ü„Éú„Çø„É≥
+    // ã≠êßèIóπÉ{É^Éì
     if (input.action(InputAction::APP_Pause)) {
         owner_.change_state((GSuint)return_scene_);
         return;
     }
 
     scale_gamepad_ += delta_time * GAMEPAD_SCALE_SPEED;
+    button_.update(delta_time);
 }
 
 void SceneGuideState::draw() const {
@@ -40,4 +48,6 @@ void SceneGuideState::draw() const {
     float scale = (std::sin(scale_gamepad_) + 1.0f) / 2.0f * GAMEPAD_MAX_SCALE;
     Canvas::draw_texture((GSuint)TextureID::GamePadIconTexture, GSvector2{ 272.0f, 178.0f }, GSrect{ 0.0f, 0.0f, 96.0f, 96.0f },
         GSvector2{ 96.0f / 2.0f, 96.0f / 2.0f }, GSvector2::one() + GSvector2::one() * scale);
+
+    button_.draw();
 }
