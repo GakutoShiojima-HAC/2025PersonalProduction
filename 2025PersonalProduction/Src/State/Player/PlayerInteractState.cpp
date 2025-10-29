a900edf2833126873f2e9cd1ce06c0e5f0ab84d2
Gakuto <G023G1022@g.neec.ac.jp>
2025-10-29 23:09:12 +0900

    攻撃時敵を見るように その2

--- 2025PersonalProduction/Src/State/Player/PlayerInteractState.cpp
+++ 2025PersonalProduction/Src/State/Player/PlayerInteractState.cpp
@@ -28,6 +28,7 @@ void PlayerInteractState::update(float delta_time) {
 
     // 攻撃入力があれば攻撃ステートへ遷移
     if (owner_.is_action(InputAction::GAME_Attack)) {
+        owner_.look_target();
         owner_.attack_start();
         owner_.change_state((GSuint)PlayerStateType::Attack, owner_.get_attack_motion(), false);
         return;
