af42f34fb4638dd0f3a5c00ac7dbc28569cf6e84
Gakuto <G023G1022@g.neec.ac.jp>
2025-12-12 03:50:20 +0900

    UIをフォントに対応

--- 2025PersonalProduction/Src/GUI/InteractUI.cpp
+++ 2025PersonalProduction/Src/GUI/InteractUI.cpp
@@ -2,6 +2,7 @@
 #include "Engine/Graphics/Canvas/Canvas.h"
 #include "Engine/Core/Actor/Actor.h"
 #include "Engine/Utils/MyString.h"
+
 // 何個UIを描画するか
 const int INTERACT_LIST{ 4 };
 
@@ -18,12 +19,15 @@ void InteractUI::draw(const std::vector<Actor*>& actors, int target_index) {
         if (index > last_index) break;
         const Actor* actor = actors[index];
         // テキストを取得
-        std::string text = MyString::utf8_to_shiftjis((index == target_index ? "選択中: " : "")) + actor->interact_text();
+        std::string text = MyString::utf8_to_shiftjis((index == target_index ? ">> " : "")) + actor->interact_text() + MyString::utf8_to_shiftjis((index == target_index ? " <<" : ""));
         // テキストを描画
-        Canvas::draw_text(
-            text,                                       // テキスト
-            GSvector2{ 1200.0f, 700.0f + (20.0f * i) },    // 描画位置
-            18.0f                                       // フォントサイズ
+        Canvas::draw_sprite_text(
+            text,                                           // テキスト
+            GSvector2{ 1200.0f, 650.0f + (20.0f * i) },     // 描画位置
+            40.0f,                                          // フォントサイズ
+            cFONT,
+            GS_FONT_NORMAL,
+            GScolor{ 1.0f, 1.0f, 1.0f, 1.0f }
         );
     }
 }
