3b0d2d30e7faa3f8c8624593468140946837a39f
Gakuto <G023G1022@g.neec.ac.jp>
2026-02-01 19:56:14 +0900

    インパクト演出の追加

--- 2025PersonalProduction/Src/Engine/Graphics/Shader/PostEffects/ImpactEffect.h
+++ 2025PersonalProduction/Src/Engine/Graphics/Shader/PostEffects/ImpactEffect.h
@@ -9,9 +9,12 @@ namespace PostEffect {
         /// インパクトエフェクトを適用する
         /// </summary>
         /// <param name="source">= エフェクトを適用したいレンダーターゲット</param>
-        /// <param name="threshold)">= 強さ</param>
+        /// /// <param name="depth">= 深度レンダーターゲット</param>
+        /// <param name="threshold">= 強さ</param>
+        /// <param name="aspect_ratio">= アスペクト比</param>
+        /// <param name="hit">= インパクト位置(スクリーン座標)</param>
         /// <returns>加工したレンダーターゲット</returns>
-        GSuint apply(GSuint source, float power);
+        GSuint apply(GSuint source, GSuint depth, float power, float aspect_ratio, const GSvector2& hit);
     }
 }
 
